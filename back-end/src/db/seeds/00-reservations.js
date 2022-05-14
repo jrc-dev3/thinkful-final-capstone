9fe63ba7b00604cd43580d4616b3a807f823b5d8
jrc <jrc.dev3@gmail.com>
2022-05-14 15:00:22 -0400

    created seed data using json

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,33 @@
+const dummyData = require("../seeds/00-reservations.json");
+
+// exports.seed = function(knex, Promise) {
+
+// };
+
 exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE");
+  return knex
+    .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
+    .then(() =>
+      knex("reservations").insert(
+        dummyData.map(
+          ({
+            first_name,
+            last_name,
+            mobile_number,
+            reservation_date,
+            reservation_time,
+            people,
+          }) => {
+            return {
+              first_name,
+              last_name,
+              mobile_number,
+              reservation_date,
+              reservation_time,
+              people,
+            };
+          }
+        )
+      )
+    );
 };
